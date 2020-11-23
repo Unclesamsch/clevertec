2b17b13fd7a9801ca266989e9bc60ab9a4b766f5
Siarhei Chekan <unclesamsch@gmail.com>
2020-11-23 19:05:39 +0300

    version2.1

--- test/js/script.js
+++ test/js/script.js
@@ -5,7 +5,6 @@ function setStyle() {
 
 }
 
-
 function addText() {
 
 	let newEl = document.createElement('p');
@@ -20,14 +19,9 @@ function addText() {
 
 function addImg() {
 
-	// let newImg = '<img src="image/4.jpeg" alt="2015-2016 Season">';
 
 	let newEl = document.createElement('img');
 		
-	// let newText = document.createTextNode('#HI5TORY');
-
-	// newEl.appendChild(newText);
-
 	let position = document.getElementById('season4');
 	position.appendChild(newEl);
 
@@ -35,9 +29,4 @@ function addImg() {
 	console.log(img);
 	img.setAttribute('src', 'image/4.jpeg');
 
-
 }
-
-// let zagolovok = '<h3> 2015-2016 Season </h3>';
-// console.log(content);
-// console.log(zagolovok);
